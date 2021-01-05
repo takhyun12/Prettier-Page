c675873876ce0bb7e48fd934d358a35dfdcc3324
takhyun12 <takhyun12@gmail.com>
2021-01-05 14:58:16 +0900

    모폴로지 연산적용 완료

--- Main.py
+++ Main.py
@@ -17,8 +17,13 @@ def Image_Process(image_path):
     ret, grayscale_image = cv.threshold(grayscale_image, 200, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)
     cv.imshow("grayscale_image", grayscale_image)
 
+    # Apply Morph Close
+    kernel = cv.getStructuringElement(cv.MORPH_RECT, (5, 5))
+    morph_closed_image = cv.morphologyEx(grayscale_image, cv.MORPH_CLOSE, kernel)
+    cv.imshow("morph_closed_image", morph_closed_image)
+
     # Find Contours
-    contours, hierarchy = cv.findContours(grayscale_image, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+    contours, hierarchy = cv.findContours(morph_closed_image, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
     contour_sizes = [(cv.contourArea(contour), contour) for contour in contours]
     biggest_contour = max(contour_sizes, key=lambda x: x[0])[1]
 
@@ -26,24 +31,20 @@ def Image_Process(image_path):
     cv.drawContours(contour_image, [biggest_contour], 0, (0, 0, 255), 2)
     cv.imshow('contour_image', contour_image)
 
-    # Crop Image
-    x, y, w, h = cv.boundingRect(biggest_contour) * np.array([ratio, ratio, ratio, ratio])
-    x = int(x)
-    y = int(y)
-    w = int(w)
-    h = int(h)
-    # cropped_image = source_image[y: y + h, x: x + w]
+    # Crop Image with source_image
+    x, y, w, h = map(int, cv.boundingRect(biggest_contour) * np.array([ratio, ratio, ratio, ratio]))
+    cropped_image = source_image[y: y + h, x: x + w]
 
-    # cv.imshow(image_path, cropped_image)
+    cv.imshow(image_path, cropped_image)
     cv.waitKey(0)
     cv.destroyAllWindows()
 
     # Save Image
-    # directory_name = '../Result'
-    # image_name = os.path.basename(image_path)
-    # result_image_path = os.path.join(directory_name, image_name)
-    # cv.imwrite(result_image_path, cropped_image)
-    # print('[>] ' + result_image_path)
+    directory_name = '../Result'
+    image_name = os.path.basename(image_path)
+    result_image_path = os.path.join(directory_name, image_name)
+    cv.imwrite(result_image_path, cropped_image)
+    print('[>] ' + result_image_path)
 
 
 class PrettierPage:
@@ -57,11 +58,16 @@ class PrettierPage:
     @staticmethod
     def Process_Directory(directory_path):
         image_list = os.listdir(directory_path)
+        allow_extension_list = [".TIF", ".GIF", ".JPG", ".JPEG"]
         for image_name in image_list:
             image_path = os.path.join(directory_path, image_name)
-            Image_Process(image_path)
+            image_extension = os.path.splitext(image_name)[1]
+
+            # Extension Check
+            if image_extension in allow_extension_list:
+                Image_Process(image_path)
 
 
 if __name__ == '__main__':
-    # PrettierPage.Process_File('../Images/0001-001.TIF')
-    PrettierPage.Process_Directory('../Images')
+    PrettierPage.Process_File('../Images/0001-055.TIF')
+    # PrettierPage.Process_Directory('../Images')
