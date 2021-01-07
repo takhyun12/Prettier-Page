1bec0ebe513c8dce752d147c7c50faf5ef06b0aa
takhyun12 <takhyun12@gmail.com>
2021-01-07 12:55:29 +0900

    함수 구조 변경

--- Main.py
+++ Main.py
@@ -1,3 +1,4 @@
+from typing import List
 import cv2 as cv
 import os
 import imutils
@@ -11,76 +12,78 @@ Note.
 '''
 
 
-def Image_Process(image_path):
-    # Image Read
-    source_image = cv.imread(image_path)
-    ratio = source_image.shape[0] / 500.0
-    copied_image = source_image.copy()
-    copied_image = imutils.resize(copied_image, height=500)
-    # cv.imshow("copied_image", copied_image)
-
-    # Apply GaussianBlur + OTSU-Thresholding
-    grayscale_image = cv.cvtColor(copied_image, cv.COLOR_BGR2GRAY)
-    grayscale_image = cv.GaussianBlur(grayscale_image, (5, 5), 0)
-    # cv.imshow("blur_image", grayscale_image)
-
-    ret, grayscale_image = cv.threshold(grayscale_image, 200, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)
-    # cv.imshow("threshold_image", grayscale_image)
-
-    # Apply Morph Open (Erosion(1 iter) => open(Erosion => Dilation))
-    kernel = cv.getStructuringElement(cv.MORPH_RECT, (5, 5))
-    grayscale_image = cv.erode(grayscale_image, kernel, iterations=1)
-    # cv.imshow("eroded_image", grayscale_image)
-
-    morph_opened_image = cv.morphologyEx(grayscale_image, cv.MORPH_OPEN, kernel)
-    # cv.imshow("morph_opened_image", morph_opened_image)
-
-    # Find Contours
-    contours, hierarchy = cv.findContours(morph_opened_image, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-    contour_sizes = [(cv.contourArea(contour), contour) for contour in contours]
-    biggest_contour = max(contour_sizes, key=lambda x: x[0])[1]
-
-    # contour_image = copied_image.copy()
-    # cv.drawContours(contour_image, [biggest_contour], 0, (0, 0, 255), 2)
-    # cv.imshow('contour_image', contour_image)
-
-    # Crop Image with source_image
-    x, y, w, h = map(int, cv.boundingRect(biggest_contour) * np.array([ratio, ratio, ratio, ratio]))
-    result_image = source_image[y: y + h, x: x + w]
-
-    # cv.imshow(image_path, cropped_image)
-    # cv.waitKey(0)
-    # cv.destroyAllWindows()
-
-    # Save Image
-    directory_name = '../Result'
-    image_name = os.path.basename(image_path)
-    result_image_path = os.path.join(directory_name, image_name)
-    cv.imwrite(result_image_path, result_image)
-    print('[>] ' + result_image_path)
-
-
 class PrettierPage:
-    def __init__(self):
-        pass
+    def __init__(self, image_path):
+        # Input Check
+        if not image_path:
+            print('[>] 정상적인 파일입력이 아닙니다.')
+            return
 
-    @staticmethod
-    def Process_File(image_path):
-        Image_Process(image_path)
+        # Parse image name and extension
+        image_name = os.path.basename(image_path)  # type: str
+        image_extension = os.path.splitext(image_name)[1].upper()  # type: str
 
-    @staticmethod
-    def Process_Directory(directory_path):
-        image_list = os.listdir(directory_path)
-        allow_extension_list = [".TIF", ".GIF", ".JPG", ".JPEG"]
-        for image_name in image_list:
-            image_path = os.path.join(directory_path, image_name)
-            image_extension = os.path.splitext(image_name)[1].upper()
+        # Extension Check for Prevent Exception
+        allow_extension_list = [".TIF", ".GIF", ".JPG", ".JPEG"]  # type: List[str]
 
-            # Extension Check
-            if image_extension in allow_extension_list:
-                Image_Process(image_path)
+        # Image Read and assignment self variable
+        if image_extension in allow_extension_list:
+            self.source_image = cv.imread(image_path)  # type: cv
+            self.image_path = image_path
+
+    def Make_Pretty(self):
+        # Image Resizing
+        ratio = self.source_image.shape[0] / 500.0
+        copied_image = self.source_image.copy()
+        copied_image = imutils.resize(copied_image, height=500)
+        # cv.imshow("copied_image", copied_image)
+
+        # Apply GaussianBlur + OTSU-Thresholding
+        grayscale_image = cv.cvtColor(copied_image, cv.COLOR_BGR2GRAY)
+        grayscale_image = cv.GaussianBlur(grayscale_image, (5, 5), 0)
+        # cv.imshow("blur_image", grayscale_image)
+
+        ret, grayscale_image = cv.threshold(grayscale_image, 200, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)
+        # cv.imshow("threshold_image", grayscale_image)
+
+        # Apply Morph Open Make_Pretty(Erosion(1 iter) => open(Erosion => Dilation))
+        kernel = cv.getStructuringElement(cv.MORPH_RECT, (5, 5))
+        grayscale_image = cv.erode(grayscale_image, kernel, iterations=1)
+        # cv.imshow("eroded_image", grayscale_image)
+
+        morph_opened_image = cv.morphologyEx(grayscale_image, cv.MORPH_OPEN, kernel)
+        # cv.imshow("morph_opened_image", morph_opened_image)
+
+        # Find Contours
+        contours, hierarchy = cv.findContours(morph_opened_image, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+        contour_sizes = [(cv.contourArea(contour), contour) for contour in contours]
+        biggest_contour = max(contour_sizes, key=lambda value: value[0])[1]
+
+        # contour_image = copied_image.copy()
+        # cv.drawContours(contour_image, [biggest_contour], 0, (0, 0, 255), 2)
+        # cv.imshow('contour_image', contour_image)
+
+        # Crop Image with source_image
+        x, y, w, h = map(int, cv.boundingRect(biggest_contour) * np.array([ratio, ratio, ratio, ratio]))
+        result_image = self.source_image[y: y + h, x: x + w]
+
+        # cv.imshow(image_path, cropped_image)
+        # cv.waitKey(0)
+        # cv.destroyAllWindows()
+
+        # Save Image
+        directory_name = '../Result'
+        image_name = os.path.basename(self.image_path)
+        result_image_path = os.path.join(directory_name, image_name)
+        cv.imwrite(result_image_path, result_image)
+        print('[>] ' + result_image_path)
 
 
 if __name__ == '__main__':
-    PrettierPage.Process_File('../Images/0028.TIF')
-    # PrettierPage.Process_Directory('../Images')
+    PrettierPage('../Images/0028.TIF').Make_Pretty()
+
+    # directory_path = '../Images'
+    # image_list = os.listdir(directory_path)
+    # for image_name in image_list:
+        # image_path = os.path.join(directory_path, image_name)
+        # PrettierPage(image_path)
